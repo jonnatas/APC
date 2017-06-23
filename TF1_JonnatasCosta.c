f56f0b74350e1e27fd99a2cbe282e38160ae217f
jonnatas <jonatas_lenon@hotmail.com>
2017-06-23 17:01:41 -0300

    colors

--- TF1_JonnatasCosta.c
+++ TF1_JonnatasCosta.c
@@ -16,6 +16,14 @@ Saida: candidatos cadastrados.
 #define MAXTAMANHONOME 50
 #define MAX_TAMANHO_LEGENDA 90
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 typedef struct Candidato{
 	char nome[MAXTAMANHONOME];
 	int lavaJato;
@@ -29,15 +37,17 @@ typedef struct Legenda{
 
 int modificar(Legenda *legenda, int tamanho);
 int cadastro(Legenda *legenda, int tamanho);
-void exibirLegenda(Legenda *legenda, int tamanho);
-void exibirLegendaLavajato(Legenda *legenda, int tamanho);
+int exibirLegenda(Legenda *legenda, int tamanho);
+int exibirLegendalavaJato(Legenda *legenda, int tamanho);
 void validarNumero(char numeroLegenda[10], Legenda *legenda, int posicao);
 void validarNome(Legenda *legenda, int posicao);
 void validarSigla(Legenda *legenda, int posicao);
 void verificarSiglaVazio(Legenda *legenda, int posicao);
-int validarSituacao(int lavaJato);
+int validarSituacao(char lavaJato[3]);
 void zerarNumeros(Legenda *legenda);
 int inicializarLegenda(Legenda *legenda);
+int apagarArquivo();
+void removerPulaLinha(char *palavra);
 
 //Variavel global para setar as repetições e evitar um laço para verificar as mesmas o(n) na busca
 int numero_repeticoes[100];
@@ -80,8 +90,11 @@ int main(void)
 				break;
 			case 4:
 				system("clear");
-				exibirLegendaLavajato(legenda, tamanho);
+				exibirLegendalavaJato(legenda, tamanho);
 
+			case 5:
+				system("clear");
+				apagarArquivo();
 				break;
 		}
 	}while(opcao!=0);
@@ -98,7 +111,7 @@ int inicializarLegenda(Legenda *legenda){
 	if(!(arquivo=fopen("legenda.bin", "r")))
 		arquivo=fopen("legenda.bin", "w+");
 	if(arquivo==NULL){
-		printf("Error ao abrir o arquivo");
+		printf(ANSI_COLOR_RED "\n\tError ao abrir o arquivo \n" ANSI_COLOR_RESET );
 		return 1;
 	}
 	while(!feof(arquivo)){
@@ -117,9 +130,26 @@ int inicializarLegenda(Legenda *legenda){
 //Objetivo: Modificar dados de uma legenda  
 //Entrada: legenda e posicao da legenda.
 //Retorno: nenhum.
+int apagarArquivo(){
+	FILE *file;
+        file = fopen("legenda.bin", "w");
+	if(file	== NULL){
+		printf(ANSI_COLOR_RED "\n\tError ao apagar o arquivo!!!\n" ANSI_COLOR_RESET );
+		return 1;
+	}
+	else{
+		printf("\nArquivo apagado com sucesso, um novo arquivo foi criado!!!\n\n");
+	}
+	fclose(file);
+	return 0;
+}
+//Objetivo: Modificar dados de uma legenda  
+//Entrada: legenda e posicao da legenda.
+//Retorno: nenhum.
 int modificar(Legenda *legenda, int tamanho){
 	int i;
 	int numeroLegenda;
+	char lavaJato[3];
 	getchar();
 	printf("Insira o numero da legenda do candidato");
 	scanf("%d", &numeroLegenda);
@@ -139,9 +169,9 @@ int modificar(Legenda *legenda, int tamanho){
 			fgets(legenda[i].sigla, MAXPARTIDO, stdin);
 			validarSigla(legenda, i);
 
-			printf("\tSituação na lavajato (%c), inserir nova: ", legenda[i].candidato.lavaJato);
-			legenda[i].candidato.lavaJato = getchar();
-			validarSituacao(legenda[i].candidato.lavaJato);
+			printf("\tSituação na lavaJato (%c), inserir nova: ", legenda[i].candidato.lavaJato);
+			fgets(lavaJato, 3, stdin);
+			legenda[i].candidato.lavaJato= validarSituacao(lavaJato);
 			return 1;
 		}
 	}
@@ -157,6 +187,8 @@ int cadastro(Legenda *legenda, int tamanho){
 	int i=0;
 	char encerrar;
 	char numeroLegenda[10];
+	char lavaJato[3];
+
 	FILE *file = fopen("legenda.bin", "ab+");
 
 	printf("Insira os dados dos canditados: \n");
@@ -184,9 +216,9 @@ int cadastro(Legenda *legenda, int tamanho){
 		fgets(legenda[i].sigla, MAXPARTIDO, stdin);
 		validarSigla(legenda, i);
 
-		printf("\tSituação na lavajato: ");
-		legenda[i].candidato.lavaJato = getchar();
-		validarSituacao(legenda[i].candidato.lavaJato);
+		printf("\tSituação na lavaJato: ");
+		fgets(lavaJato, 3, stdin);
+		legenda[i].candidato.lavaJato= validarSituacao(lavaJato);
 
 		//fwrite(&legenda[i], sizeof(legenda[i]), 1, file);
 
@@ -196,7 +228,6 @@ int cadastro(Legenda *legenda, int tamanho){
 		fprintf(file,"%s\n", legenda[i].sigla);
 		fprintf(file,"%c\n", legenda[i].candidato.lavaJato);
 
-		getchar();
 		printf("Encerrar (S-sim)");
 		encerrar = getchar();
 		if(encerrar=='S' || encerrar=='s')
@@ -211,8 +242,12 @@ int cadastro(Legenda *legenda, int tamanho){
 //Objetivo: Exibir candidatos com problemas na Lava Jato 
 //Entrada: legenda e posicao da legenda.
 //Retorno: nenhum.
-void exibirLegendaLavajato(Legenda *legenda, int tamanho){
-	int i;
+int exibirLegendalavaJato(Legenda *legenda, int tamanho){
+	int i;	
+	if(tamanho==0){
+		printf(ANSI_COLOR_RED "\n\tNenhum dado encontrado\n" ANSI_COLOR_RESET );
+		return 0;
+	}
 	for(i=0; i<=tamanho; i++){
 		if(legenda[i].candidato.lavaJato == 'S'){
 			printf("Candidato %d\n", i+1);
@@ -222,14 +257,20 @@ void exibirLegendaLavajato(Legenda *legenda, int tamanho){
 			printf("\tlava Jato: %c\n", legenda[i].candidato.lavaJato);
 		}
 	}
+	return 1;
 }
 
 
 //Objetivo: Exibir todas as legendas  
 //Entrada: legenda e posicao da legenda.
 //Retorno: nenhum.
-void exibirLegenda(Legenda *legenda, int tamanho){
+int exibirLegenda(Legenda *legenda, int tamanho){
 	int i;
+	if(tamanho==0){
+		printf(ANSI_COLOR_RED "\n\tNenhum dado encontrado\n" ANSI_COLOR_RESET );
+
+		return 0;
+	}
 	for(i=0; i<=tamanho; i++){
 		printf("Candidato %d\n", i+1);
 		printf("\tNunero: %d\n",legenda[i].candidato.numeroLegenda);
@@ -237,6 +278,7 @@ void exibirLegenda(Legenda *legenda, int tamanho){
 		printf("\tSigla: %s\n", legenda[i].sigla);
 		printf("\tlava Jato: %c\n", legenda[i].candidato.lavaJato);
 	}
+	return 1;
 }
 
 //Objetivo: Validar o nome do usuário 
@@ -245,7 +287,7 @@ void exibirLegenda(Legenda *legenda, int tamanho){
 void validarNome(Legenda *legenda, int posicao){
 	legenda[posicao].candidato.nome[strlen(legenda[posicao].candidato.nome) -1] = '\0' ;
 	while(strlen(legenda[posicao].candidato.nome)<=0  || legenda[posicao].candidato.nome[0]=='\n' || legenda[posicao].candidato.nome[0]==' '){
-		printf("Error. Nome Vazio, insira novamente: ");
+		printf(ANSI_COLOR_RED "\n\tError. Nome Vazio, insira novamente: \n" ANSI_COLOR_RESET );
 		fgets(legenda[posicao].candidato.nome, MAXTAMANHONOME, stdin);
 		legenda[posicao].candidato.nome[strlen(legenda[posicao].candidato.nome) -1] = '\0' ;
 	}
@@ -255,11 +297,11 @@ void validarNome(Legenda *legenda, int posicao){
 //Entrada: legenda e posicao da legenda.
 //Retorno: nenhum.
 void verificarSiglaVazio(Legenda *legenda, int posicao){
-	legenda[posicao].sigla[strlen(legenda[posicao].sigla) -1] = '\0' ;
+	removerPulaLinha(legenda[posicao].sigla);
 	while(strlen(legenda[posicao].sigla)<=0  || legenda[posicao].sigla[0]=='\n' || legenda[posicao].sigla[0]==' '){
-		printf("Error. Sigla Vazio, insira novamente: ");
+		printf(ANSI_COLOR_RED "\n\tError. Sigla Vazio, insira novamente: \n" ANSI_COLOR_RESET );
 		fgets(legenda[posicao].sigla, MAXPARTIDO, stdin);
-		legenda[posicao].sigla[strlen(legenda[posicao].sigla) -1] = '\0' ;
+		removerPulaLinha(legenda[posicao].sigla);
 	}
 }
 
@@ -273,7 +315,7 @@ void validarSigla(Legenda *legenda, int posicao){
 		for(i=0; i<posicao; i++){
 			while(strcmp(legenda[posicao].sigla,legenda[i].sigla)==0){
 				i=0;
-				printf("Error valor existente insira novamente: ");
+				printf(ANSI_COLOR_RED "\n\tError valor existente insira novamente: \n" ANSI_COLOR_RESET );
 				fgets(legenda[posicao].sigla, MAXPARTIDO, stdin);
 				verificarSiglaVazio(legenda, posicao);
 			}
@@ -281,17 +323,27 @@ void validarSigla(Legenda *legenda, int posicao){
 	}
 }
 
-//Objetivo: validar situação na lavajato 
+//Objetivo: Remover pula linha
+//Entrada: Uma string qualquer
+//Retorno: String sem pula linha
+void removerPulaLinha(char *palavra){
+	int tamanho = strlen(palavra);
+	palavra[tamanho-1]='\0';
+}
+
+//Objetivo: validar situação na lavaJato 
 //Entrada: situação
 //Retorno: situação devidamente validada
-int validarSituacao(int lavajato){
-
-	while(lavajato != 'S' && lavajato != 'N'){
-		printf("Error, valor incorreto, insira (S-Sim ou N-Não): ");
-		lavajato = getchar();	
+int validarSituacao(char lavaJato[3]){
+	removerPulaLinha(lavaJato);
+	
+	while((lavaJato[0] != 'S' && lavaJato[0] != 'N') || strlen(lavaJato)>1 ){
+		printf(ANSI_COLOR_RED "\n\tError, valor incorreto, insira (S-Sim ou N-Não): \n" ANSI_COLOR_RESET );
+		fgets(lavaJato, 3, stdin);
+		removerPulaLinha(lavaJato);
 	}
 
-	return lavajato;
+	return lavaJato[0];
 }
 
 //Objetivo: validar numero da legenda
@@ -302,7 +354,7 @@ void validarNumero(char numeroLegenda[10], Legenda *legenda, int posicao){
 	int numeroLegendaConvertido = atoi(numeroLegenda);
 
 	while(numeroLegendaConvertido<10 || numeroLegendaConvertido>100 || numero_repeticoes[numeroLegendaConvertido]){
-		printf("Valor errado. insira novamente: ");
+		printf(ANSI_COLOR_RED "\n\tError. Valor errado. insira novamente: " ANSI_COLOR_RESET );
 		fgets(numeroLegenda, 10, stdin);
 		numeroLegendaConvertido = atoi(numeroLegenda);
 	}
